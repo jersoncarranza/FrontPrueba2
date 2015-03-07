ea3b012fe834473d689bcf40b2b00e9b7ca502ac
Jerson Carranza Lucas <jerson_carranza2@hotmail.com>
2015-03-06 20:04:25 -0500

    master

---
+++ countriesFactory.js
@@ -0,0 +1,19 @@
+angular.module('countriesFactory', [])
+       .factory('countries', function($http){
+  return {
+    list: function (callback){
+      $http({
+        method: 'GET',
+        url: 'http://127.0.0.1:7000/files/list',
+        cache: true
+      }).success(callback);
+    },
+    find: function(id, callback){
+      $http({
+        method: 'GET',
+        url: 'http://127.0.0.1:7000/files/see/' + id,
+        cache: true
+      }).success(callback);
+    }
+  };
+});
